3384b54b074803e1c44b497ccde8c8a7d342a336
Joachim Veulemans <joachim@cloudclass.be>
2019-10-21 00:57:53 +0200

    set origin correctly

--- backend/application.py
+++ backend/application.py
@@ -8,7 +8,7 @@ import logging
 
 origin = os.getenv('ORIGIN')
 if origin is None:
-    origin = 'http://localhost:4200'
+    origin = 'https://honourlogs.jocawebs.be:443'
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, resources={
